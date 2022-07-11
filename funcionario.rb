a71e34be70eb8383a295231e11499429b7365f21
Henrique Santana <hsantana.sousass@gmail.com>
2022-07-11 20:06:41 -0300

    Add campo de id no model e dois métodos

--- funcionario.rb
+++ funcionario.rb
@@ -1,8 +1,10 @@
+require './bd.rb'
+
 class Funcionario
-    attr_accessor(:name, :last_name, :age, :salary, :address, :sector, :enrollment)
+    attr_accessor(:id, :name, :last_name, :age, :salary, :address, :sector, :enrollment)
 
     def initialize(params)
-        # @id = id ++
+        @id = Bd.funcionario_id
         @name = params[:name]
         @last_name = params[:last_name]
         @age = params[:age]
@@ -10,4 +12,25 @@ class Funcionario
         @address = params[:sector]
         @enrollment = params[:enrollment]
     end
+
+    def nome_completo()
+        return `#{@name} #{@last_name}`
+    end
+
+    def numero_dependentes()
+        numero_dependentes = 0
+        @dependentes = Bd.class_variable_get(:@@dependentes)
+        @dependentes.each do |dependente|
+            puts dependente
+            if dependente.id == self.id
+                puts self
+                numero_dependentes += 1
+            end
+        end
+
+        return numero_dependentes
+    end
 end
+
+# a = Funcionario.new({:name => "Henrique", :last_name => "Santana", :age => 20, :salary => 2000, :address => "Rua", :sector => "Financeiro", :enrollment => "131243"})
+
