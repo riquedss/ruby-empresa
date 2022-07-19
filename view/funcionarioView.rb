b91b0e7a33829820ddda9c92dc2319eb7617d004
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-19 02:04:35 -0300

    alteração nos inputs e implementado 4 caminho de routes no crud de funcionarios

--- view/funcionarioView.rb
+++ view/funcionarioView.rb
@@ -1,43 +1,57 @@
-require '../controller/funcionariosController'
+require './controller/funcionariosController'
 
 def view_create_funcionario()
   print('Name: ')
-  name = gets
+  name = gets.chomp
   print('Last Name: ')
-  last_name = gets
+  last_name = gets.chomp
   print('Age: ')
-  age = gets.to_i
+  age = gets.chomp.to_i
   print('Salary: ')
-  salary = gets.to_f
+  salary = gets.chomp.to_f
   print('Address: ')
   address = gets
   print('Sector: ')
-  sector = gets
+  sector = gets.chomp
   print('Enrollment: ')
-  enrollment = gets  
+  enrollment = gets.chomp 
 
   return {
-      :name => name, 
-      :last_name => last_name, 
-      :age => age, 
-      :salary => salary, 
-      :address => address, 
-      :sector => sector, 
-      :enrollment => enrollment
+    :name => name, 
+    :last_name => last_name, 
+    :age => age, 
+    :salary => salary, 
+    :address => address, 
+    :sector => sector, 
+    :enrollment => enrollment
   } 
+end
+
+def delete_view_funcionario() 
+  print "id: "
+    if FuncionariosController.show(gets.chomp)
+      mensagem("Apagado")
+    else
+      mensagem("Funcionario encontrado")
+    end
 end
 
 def funcionario_routes(comando)
   if comando == "/create/funcionario"
       funcionario = view_create_funcionario()
-      create(funcionario)
+      FuncionariosController.create(funcionario)
   elsif comando == "/index/funcionario"
-      funcionario = index()
-      mensagem_lista_tabela(funcionario)
+      funcionario = FuncionariosController.index()
+      mensagem_lista_funcionarios(funcionario)
   elsif comando == "/show/funcionario"
-
+    print "id: "
+    funcionario = FuncionariosController.show(gets.chomp.to_i)
+    puts "teste show"
+    puts funcionario.class
+    mensagem_lista_funcionarios([funcionario])
   elsif comando == "/update/funcionario"
   elsif comando == "/delete/funcionario"
+    delete_view_funcionario()  
   else
     return false
   end
