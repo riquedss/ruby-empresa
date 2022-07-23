7cf7394d077ce965aa7e425c093fa610c3995f1d
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-23 14:46:22 -0300

    Alteração na estrutura da view

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require './view/mensagemView.rb'
 mensagem_abertura()
 menu = true
 while menu do 
-    if(!index_comandos())
+    if(!index_comandos_models())
         print "saiu\n"
         menu = false
     end 
